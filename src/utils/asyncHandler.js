c7ecb48e7f42f460eef388dd1846f4000c4f9fc0
Rihanmulani12 <rihanmulani18@gmail.com>
2024-01-29 12:47:25 +0530

    all setup config and postman

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,11 @@
-const asyncHandler = (reqHandler) => {
-   (req, res, next) =>{
-    Promise.resolve(reqHandler(req, res,next)).catch((err) => next(err))
+const asyncHandler = (requestHandler) => {
+   return (req, res, next) => {
+       Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
    }
 }
 
-export default asyncHandler;
+
+export { asyncHandler }
 
 // const asyncHandler = (fn) => async(req, res, next) => {
 //     try {
